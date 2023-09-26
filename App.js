764fd971058bfab2d9fb03d8d5d67df5fc610707
NguyenChiCuong <20020331.cuong@student.iuh.edu.vn>
2023-09-26 14:58:41 +0700

    firstcommit

--- App.js
+++ App.js
@@ -1,20 +1,34 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-
+import { StyleSheet, Text, View,Button ,Image} from 'react-native';
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#ffff',
+         alignItems: 'center',
+        justifyContent: 'center',
+    },
+    // text:{
+    //     color: '#111',
+    //     textAlign: 'center',
+    //     fontFamily: 'Roboto',
+    //     fontSize: '25px',
+    //     fontStyle: normal,
+    //     fontWeight: 700,
+    //     lineHeight: normal,
+   //}
+});
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return ( < View style = { styles.container }>
+        <svg  xmlns="http://www.w3.org/2000/svg" width="140" height="140" viewBox="0 0 140 140" fill="none">
+        <circle cx="70" cy="70" r="62.5" stroke="black" stroke-width="15"/>
+        </svg>
+        {/* <View>
+            <Text style={styles.text}>GROW YOUR BUSINESS</Text>
+        </View> */}
+        </View>
+       
+   
+    );
+
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
